1ef8bc7a8e23cd5a80b039c10925ef8b486c6dbd
petite_marie <minaeva.mav@gmail.com>
2021-10-01 15:49:22 +0200

    added translation and alignmment

--- pipeline_for_DNA_protein_mapping.R
+++ pipeline_for_DNA_protein_mapping.R
@@ -9,51 +9,143 @@ for (i in 0:2){
 
 
 
+library(spgs)
 
-
-
-## reading data
-data = read.table("~/splicing_project/significant_sQTL_GWAS_coloc_events.tsv", header=T)
-
-## main cycle
-for (j in 1:2){
-gene_name = needed_coords = unlist(strsplit(data$phenotype_id[j],split = "_"))[1]
-bash = c("bash ~/splicing_project/retrieve_nucleotide_sequence_from_genename.sh -g", gene_name)
-bash =  paste(bash, collapse=" ")
-print(bash)
-## exon extraction
-system(bash)
-output = read.table("~/splicing_project/output.txt", sep="\n")
+## parsing transeq output
+transeq_parse <- function(input) {
+        
+        proteins = character(0)
+        for (i in 1:nrow(input)){
+                if (startsWith(input[i,], ">_")){
+                        pars = unlist(strsplit(input[i,],split = ":"))[2]
+                        pars = unlist(strsplit(pars,split = "-"))
+                        proteins = c(proteins, temp)
+                        temp = character(0)
+                }else{
+                        temp = c(temp, input[i,])
+                        temp = paste(temp, collapse="")
+                }
+                if (i == nrow(input)){
+                        proteins = c(proteins, temp) 
+                }
+        }
+        return(proteins)
+}
 
 ## parsing bedtools output
-starts = character(0)
-stops = character(0)
-temp = character(0)
-exons = character(0)
-n=length(rownames(output))
-for (i in 1:n){
-        if (startsWith(output[i,], ">chr")){
-                pars = unlist(strsplit(output[i,],split = ":"))[2]
-                pars = unlist(strsplit(pars,split = "-"))
-                exons = c(exons, temp)
-                starts = c(starts, pars[1])
-                stops = c(stops, pars[2])
-                temp = character(0)
-        }else{
-                temp = c(temp, output[i,])
-                temp = paste(temp, collapse="")
+bedtools_parse = function(input){
+        sta = character(0)
+        sto = character(0)
+        temp = character(0)
+        exo = character(0)
+        for (i in 1:nrow(input)){
+                if (startsWith(input[i,], ">chr")){
+                        pars = unlist(strsplit(input[i,],split = ":"))[2]
+                        pars = unlist(strsplit(pars,split = "-"))
+                        exo = c(exo, temp)
+                        sta = c(sta, pars[1])
+                        sto = c(sto, pars[2])
+                        temp = character(0)
+                }else{
+                        temp = c(temp, input[i,])
+                        temp = paste(temp, collapse="")
+                }
+                if (i == nrow(input)){
+                        exo = c(exo, temp) 
+                }
         }
-        if (i == n){
-                exons = c(exons, temp) 
-        }
+        return(list(sta, sto, exo))
 }
-print(data$Exon_coord[j])
-needed_coords = unlist(strsplit(data$Exon_coord[j],split = "_"))[2:3]
-print(needed_coords)
-print(starts)
-print(stops)
-print(match(needed_coords[1], starts))
-print(match(needed_coords[2], stops))
 
+
+
+
+
+
+
+## Setting directory
+setwd("~/splicing_project/")
+
+## reading data
+data = read.table("significant_sQTL_GWAS_coloc_events.tsv", header=T)
+
+## main cycle
+for (j in 1:1){
+        gene_name = needed_coords = unlist(strsplit(data$phenotype_id[j],split = "_"))[1]
+        bash = c("bash retrieve_nucleotide_sequence_from_genename.sh -g", gene_name)
+        bash =  paste(bash, collapse=" ")
+        print(bash)
+        ## all gene exons extraction
+        system(bash)
+        #file.show("~/splicing_project/output.txt")
+        
+        ## bedtools parsing
+        output = read.table("output.txt", sep="\n")
+        res = bedtools_parse(output)
+        starts = unlist(res[1])
+        stops = unlist(res[2])
+        exons = unlist(res[3])
+        
+        ## needed exon extraction
+        print(data$Exon_coord[j])
+        needed_coords = unlist(strsplit(data$Exon_coord[j],split = "_"))[2:3]
+        print(needed_coords)
+        print(stops)
+        print(match(needed_coords[2], stops))
+        #rm(output)
+
+        needed_exon = exons[match(needed_coords[2], stops)]
+        print(needed_exon)
+        write(needed_exon, "exon_seq.fa")
+        
+        ## Translation
+        system("transeq -frame 6 exon_seq.fa protein_sense_seq.txt")
+        system("cat protein_sense_seq.txt")
+        
+        ## finding complement sequence
+        comp_needed_exon = complement(needed_exon)
+        
+        ## complement translation
+        write(comp_needed_exon, "compl_exon_seq.fa")
+        system("transeq -frame 6 compl_exon_seq.fa compl_protein_sense_seq.txt")
+        system("cat compl_protein_sense_seq.txt")
+        
+        ## parsing ALphaFold database
+        
+        trial_ref_seq = "PIGRLETPVVGDVLPQGVPPVHRLPVHAVVAVLLYHALGLTLEGLHG*VLPPWPEVPVLVILPS"
+        write(trial_ref_seq, "trial_prot_ref.fa")
+        
+        ## Preprocessing for alignment
+        output = read.table("protein_sense_seq.txt")
+        proteins = transeq_parse(output)
+        
+        ## Aligning sense proteins
+        for (k in 1:length(proteins)){
+                print("Sense")
+                print(proteins[k])
+                write(proteins[k], "temp_prot.fa")
+                out_path = paste(c("alignment_sense_", as.character(k),".water"), collapse="")
+                water = c("water -gapopen 10.0 -gapextend 0.5", "temp_prot.fa", "trial_prot_ref.fa", out_path)
+                water =  paste(water, collapse=" ")
+                system(water)
+                
+        }
+        
+        ## Preprocessing for alignment
+        output = read.table("compl_protein_sense_seq.txt")
+        proteins = transeq_parse(output)
+        print(proteins)
+        ## Aligning antisense proteins
+        for (k in 1:length(proteins)){
+                print("Anti")
+                print(proteins[k])
+                write(proteins[k], "temp_prot.fa")
+                out_path = paste(c("alignment_antisense_", as.character(k),".water"), collapse="")
+                water = c("water -gapopen 10.0 -gapextend 0.5", "temp_prot.fa", "trial_prot_ref.fa", out_path)
+                water =  paste(water, collapse=" ")
+                system(water)
+                
+        }
+        
 }
 
