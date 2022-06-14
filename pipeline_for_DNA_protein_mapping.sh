7c3472e6343a252ad4ba8b06b99ae32c3ab98c27
petite_marie <minaeva.mav@gmail.com>
2022-06-14 11:37:55 +0200

    revised pipeline

--- pipeline_for_DNA_protein_mapping.sh
+++ pipeline_for_DNA_protein_mapping.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 
-# pretraining biomaRt
-Rscript get_biomart_pretrained.R
+module load R_packages/4.1.1
+module load bioinfo-tools
+module load blast
 
-for i in {1..3788}
+for i in {1..5232}
 	do
 	echo This is round $i
 	# Exon extraction
@@ -12,53 +13,15 @@ for i in {1..3788}
 	Rscript get_needed_exons.R $i
 	Rscript get_needed_exon.R $i
 
-	# Translation
-	echo Translation
-	transeq -frame 6 -trim T exon_seq.fa protein_sense_seq.txt
-
-	# Complement sequence
-	echo Complement sequence
-	revseq exon_seq.fa -noreverse -complement -outseq compl_exon_seq.fa
-
-	# Translation of the ccomplement sequence
-	echo Translation of the ccomplement sequence
-	transeq -frame 6 -trim T compl_exon_seq.fa compl_protein_antisense_seq.txt
-
-	# Parsing AlphaFold
-	echo Parsing AlphaFold
-	Rscript get_uniprot_id_from_ensembl.R $i
-
-	# Align sense
-	echo Align sense
-	Rscript align_sense.R
-
-	# Align antisense
-	echo Align antisense
-	Rscript align_antisense.R
-
-	# Extracting best protein alignment
-	echo Extracting best protein alignment
-	Rscript get_best_score.R
-
-	# Extract protein properties
-	echo Extract protein properties
-	Rscript get_protein_properties.R
+	# blastx
+	echo blastx
+	blastx -query exon_seq.fa -db MANE_prot -out try_blastx.txt -outfmt "6 qseqid sacc sstart send sseq length evalue score"
 
 	# Combine final table
 	echo Combine final table
 	Rscript get_final_table.R
 
-
-	rm alignment_*
-	rm align_coords*
-	rm best*
-	rm compl*
-	rm exon*
-	rm gene_*
-	rm output.*
-	rm protein*
-	rm ref*
-	rm temp*
+	rm try_blastx.txt
+	rm exon_seq.fa
 	done
 
-#python3.9 try_pymol.py
