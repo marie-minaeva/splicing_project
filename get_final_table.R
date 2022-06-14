8f32964d48c425d3d908733bf47d36c60646a175
petite_marie <minaeva.mav@gmail.com>
2022-06-14 11:38:25 +0200

    revised pipeline

--- get_final_table.R
+++ get_final_table.R
@@ -1,30 +1,22 @@
+library(data.table)
+library(stringr)
+
+
 setwd("~/splicing_project/")
 
-gene_meta = readLines("gene_metadata.txt")
+data = fread("try_blastx.txt", header=F)
+print(head(data))
 
-align_meta = readLines("best_align_meta.txt")
-
-protein = readLines("best_aligned_protein.txt")
-prot_seq = readLines("protein_seq.txt")
-
-if (protein == "NOT IN ALPHAFOLD" || protein == "EXON NOT FOUND"){
-        protein_len_ali = ''
-} else {
-        protein_len_ali = nchar(protein)
-}
-prot_len =  nchar(prot_seq)
-exon = readLines("exon_meta.txt")
-prot_meta = readLines("protein_meta.txt")
-align_coords = readLines("align_coords.txt")
-
-if (file.exists("Data/output_non_sQTL.csv")){
-        input = read.csv("Data/output_non_sQTL.csv")
+if (file.exists("Data/try_new_pipeline.csv")){
+        input = read.csv("Data/try_new_pipeline.csv")
+	colnames(input) = c("GENE ID", "PROT REFSEQ", "START", "END", "SEQ", "ALIGN LENGTH", "EVAL", "SCORE", "ASN #", "CYS #")
 } else {
         input = data.frame()
 }
 
-line = c(gene_meta, exon, align_meta, prot_seq, protein, prot_len, protein_len_ali, align_coords, prot_meta)
-input = rbind(input, line)
-colnames(input) = c("GENE NAME", "PROTEIN", "ALPHAFOLD NAME", "EXON", "READ FRAME", "STRAND", "DIRECTION", "SEQ", "ALIGNED SEQ", "LENGTH", "LENGTH ALIGN", "ALIGN COORDS", "ASN #", "CYS #", "HYDROPATHICITY")
-write.csv(input, "Data/output_non_sQTL.csv", row.names = F)
-#View(input)
+line = data.table(data[1,], str_count(data[1, "V5"], pattern="C"), str_count(data[1, "V5"], pattern="N"))
+colnames(line) = c("GENE ID", "PROT REFSEQ", "START", "END", "SEQ", "ALIGN LENGTH", "EVAL", "SCORE", "ASN #", "CYS #")
+
+
+line = rbind(input, line)
+write.csv(line, "Data/try_new_pipeline.csv", row.names = F)
